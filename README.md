8efeabe9af6df952f2b6d446773d590f9f9f4279
sash04ek <a.ovcharov@gmail.com>
2016-11-21 16:42:48 +0300

    first commit

---
+++ README.md
@@ -0,0 +1,2 @@
+# aurora-module-dav-contacts
+Adds ability to work with Dav Contacts
---
+++ module.php
@@ -0,0 +1,94 @@
+<?php
+
+class DavContactsModule extends AApiModule
+{
+	public $oApiContactsManager = null;
+
+	public function init() 
+	{
+		$this->oApiContactsManager = $this->GetManager();
+		
+		$this->subscribeEvent('Contacts::CreateContact::after', array($this, 'onAfterCreateContact'));
+		$this->subscribeEvent('Contacts::UpdateContact::after', array($this, 'onAfterUpdateContact'));
+		$this->subscribeEvent('Contacts::DeleteContact::after', array($this, 'onAfterDeleteContact'));
+
+		$this->subscribeEvent('Contacts::CreateGroup::after', array($this, 'onAfterCreateGroup'));
+		$this->subscribeEvent('Contacts::UpdateGroup::after', array($this, 'onAfterUpdateGroup'));
+		$this->subscribeEvent('Contacts::DeleteGroup::after', array($this, 'onAfterDeleteGroup'));
+
+		$this->subscribeEvent('Contacts::AddContactsToGroup::after', array($this, 'onAfterAddContactsToGroup'));
+		$this->subscribeEvent('Contacts::RemoveContactsFromGroup::after', array($this, 'onAfterRemoveContactsFromGroup'));
+	}
+
+	/**
+	 * @param array $aArgs
+	 * @param array $aResult
+	 */
+	public function onAfterCreateContact(&$aArgs, &$aResult)
+	{
+		\CApi::checkUserRoleIsAtLeast(\EUserRole::NormalUser);
+	}	
+	
+	/**
+	 * @param array $aArgs
+	 * @param array $aResult
+	 */
+	public function onAfterUpdateContact(&$aArgs, &$aResult)
+	{
+		\CApi::checkUserRoleIsAtLeast(\EUserRole::NormalUser);
+	}
+	
+	/**
+	 * @param array $aArgs
+	 * @param array $aResult
+	 */
+	public function onAfterDeleteContacts(&$aArgs, &$aResult)
+	{
+		\CApi::checkUserRoleIsAtLeast(\EUserRole::NormalUser);
+	}	
+	
+	/**
+	 * @param array $aArgs
+	 * @param array $aResult
+	 */
+	public function onAfterCreateGroup(&$aArgs, &$aResult)
+	{
+		\CApi::checkUserRoleIsAtLeast(\EUserRole::NormalUser);
+	}
+	
+	/**
+	 * @param array $aArgs
+	 * @param array $aResult
+	 */
+	public function onAfterUpdateGroup(&$aArgs, &$aResult)
+	{
+		\CApi::checkUserRoleIsAtLeast(\EUserRole::NormalUser);
+	}	
+	
+	/**
+	 * @param array $aArgs
+	 * @param array $aResult
+	 */
+	public function onDeleteGroup(&$aArgs, &$aResult)
+	{
+		\CApi::checkUserRoleIsAtLeast(\EUserRole::NormalUser);
+	}
+	
+	/**
+	 * @param array $aArgs
+	 * @param array $aResult
+	 */
+	public function onAfterAddContactsToGroup(&$aArgs, &$aResult)
+	{
+		\CApi::checkUserRoleIsAtLeast(\EUserRole::NormalUser);
+	}
+	
+	/**
+	 * @param array $aArgs
+	 * @param array $aResult
+	 */
+	public function onAfterRemoveContactsFromGroup(&$aArgs, &$aResult)
+	{
+		\CApi::checkUserRoleIsAtLeast(\EUserRole::NormalUser);
+	}	
+}
