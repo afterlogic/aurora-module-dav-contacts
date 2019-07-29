91a171113ca4a58bc75aef39adee22992a6bb3f5
nadine-shi <nadine@afterlogic.com>
2019-07-29 18:13:58 +0300

    Improve web API of Core module for admin panel entities management

--- Module.php
+++ Module.php
@@ -556,8 +556,7 @@ class Module extends \Aurora\System\Module\AbstractModule
 	{
 		$oAuthenticatedUser = \Aurora\System\Api::getAuthenticatedUser();
 		
-		$oCoreDecorator = \Aurora\Modules\Core\Module::Decorator();
-		$oUser = $oCoreDecorator ? $oCoreDecorator->GetUser($aArgs['UserId']) : null;
+		$oUser = \Aurora\Modules\Core\Module::Decorator()->GetUserUnchecked($aArgs['UserId']);
 		
 		if ($oUser instanceof \Aurora\Modules\Core\Classes\User && $oAuthenticatedUser->Role === \Aurora\System\Enums\UserRole::TenantAdmin && $oUser->IdTenant === $oAuthenticatedUser->IdTenant)
 		{
